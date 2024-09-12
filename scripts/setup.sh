b098065bdbe919fd4a1341f3ed4e72cead94d94f
Yufeng Wang <wangyufe@naf-ilc-gpu01.desy.de>
2024-09-12 11:31:43 +0200

    small update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,8 @@
 echo "evnironment setup..."
 source /cvmfs/sw.hsf.org/key4hep/setup.sh -r 2024-04-12
-source /Users/yufengwang/Work/key4hep/ACTs/install/bin/this_acts.sh
-export CMAKE_PREFIX_PATH=/Users/yufengwang/Work/key4hep/ACTs/install:$CMAKE_PREFIX_PATH
-export MARLIN_DLL=$MARLIN_DLL:/Users/yufengwang/Work/key4hep/LUXE/MarlinACTS_32_Tracking/install/lib/libMarlinACTSTracking.so
+source /nfs/dust/ilc/user/wangyufe/ACTs/install/bin/this_acts.sh
+#source /Users/yufengwang/Work/key4hep/ACTs/install/bin/this_acts.sh
+export CMAKE_PREFIX_PATH=/nfs/dust/ilc/user/wangyufe/ACTs/install:$CMAKE_PREFIX_PATH
+#export CMAKE_PREFIX_PATH=/Users/yufengwang/Work/key4hep/ACTs/install:$CMAKE_PREFIX_PATH
+export MARLIN_DLL=$MARLIN_DLL:/nfs/dust/ilc/user/wangyufe/MarlinACTS_32_Tracking/install/lib/libMarlinACTSTracking.so
+#export MARLIN_DLL=$MARLIN_DLL:/Users/yufengwang/Work/key4hep/LUXE/MarlinACTS_32_Tracking/install/lib/libMarlinACTSTracking.so
